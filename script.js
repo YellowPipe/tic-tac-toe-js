422a3d29b9e7d49f1a7e9a0ccc59dc5b9c57fe0c
YellowPipe <philippdmitrienko@gmail.com>
2019-07-03 17:28:17 +0300

    basic html structure and board module prototype

---
+++ script.js
@@ -0,0 +1,25 @@
+const board = ( () => {
+	const boardArray = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'];
+	const winCombinations = [];
+	// function render() {
+	// 	const boardDiv = document.getElementById('board');
+	// 	// const squares = document.getElementsByClassName
+	// 	for (let i=0; i<boardArray.length, i++) {
+	// 		let square = document.getElementById(`sqr${i}`)
+	// 		square.innerHTML = boardArray[i]
+	// 	}
+	// }
+	function move() { 
+	}
+
+	function checkWinner(){
+
+	}
+
+	function checkTie() {
+
+	}
+
+	return {move, checkWinner, checkTie, winCombinations}
+
+} )	
