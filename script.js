76658805594d54aa633907a1c74cd9254ec7edad
Talaa Zina <talazina@gmail.com>
2019-07-04 13:58:45 +0100

    add button for a new game,add checkwinner function

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ const Player = (name,symbol) => {
 }
 const Board = ( () => {
 	const boardArray = [null, null, null, null, null, null, null, null, null];
-	const winCombinations = [];
+	const winCombinations = [
+					[0,1,2],
+					[3,4,5],
+					[6,7,8],
+					[0,3,6],
+					[1,4,7],
+					[2,5,8],
+					[0,4,8],
+					[6,4,2]
+					];
 	// function render() {
 	// 	const boardDiv = document.getElementById('board');
 	// 	// const squares = document.getElementsByClassName
@@ -13,17 +22,34 @@ const Board = ( () => {
 	// 	}
 	// }
 
-	function checkWinner(){
+	function checkWinner(symb){
+		var plays = this.boardArray.reduce((acc,val,index)=>
+			val === symb ? acc.concat(index):acc,[]);
+	
+		let won = winCombinations.reduce(function verify(acc,elm){
+			if (elm.every((val) => plays.indexOf(val) > -1 ))
+				acc = elm;	
+            return acc
+		},[]);
 
+		return won
 	}
 
 	function checkTie() {
 
 	}
 
-	return {boardArray}
+	return {boardArray,checkWinner}
 
 } )	
+
+const deleteListeners = (game) => {
+	const sqrs = document.getElementsByClassName("sqr");
+	for(let i = 0; i<sqrs.length; i++) {
+		sqrs[i].EventListener('click', function(e){game.move(e)})
+	}
+};
+
 
 const Game = ( () => {
 	const board = Board()
@@ -35,7 +61,15 @@ const Game = ( () => {
 			e.target.innerHTML = symb;			
 			board.boardArray[id] = symb
 		}
-		//check_Win_Or_Tie()
+		let comb = board.checkWinner(symb);
+		if (comb.length != 0){
+			
+			comb.forEach(function(elm) {
+				info = document.getElementById(elm.toString());
+				info.style.color = 'red'
+			} )
+			//deleteListeners(this.game);
+		}
 	}
     let symb = 'O';
     const whichSymb = () =>{symb === 'O'? symb='X':symb='O'}
@@ -48,8 +82,7 @@ const addListeners = (game) => {
 		sqrs[i].addEventListener('click', function(e){game.move(e)})
 	}
 
-}
-
+};
 
 (function () {
     // logic here
